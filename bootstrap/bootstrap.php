5f1fdbbfe2b0d4c7f8b5e6ee4ebbcb26f46505f5
Tom Rune Flo <tom@upnode.no>
2022-03-25 04:00:36 +0100

    set php ini earlier

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -25,26 +25,23 @@ $config = require $basePath.'/config/config.php';
 // create application config container
 $applicationConfig = new ApplicationConfig($config['config']);
 
+// set php ini settings
+$applicationConfig->setPhpIniFromConfig($config['phpIni']);
+
 // create service container, add the application config instance
 $serviceContainer = new ServiceContainer();
 $serviceContainer->set(ApplicationConfig::class, $applicationConfig);
-
-// set php ini settings
-$applicationConfig->setPhpIniFromConfig($config['phpIni']);
 
 // add all configured service providers to service container
 foreach ($config['services'] as $serviceProviderClass) {
     $serviceContainer->addServiceProvider(new $serviceProviderClass($applicationConfig));
 }
 
-// get the autowire service, add service container to autowire containers
-
 /** @var \Mikrofraim\Service\Autowire\Autowire */
 $autowire = $serviceContainer->get(Autowire::class);
 $autowire->addContainer($serviceContainer);
 
 $middlewareQueue = [];
-
 foreach ($config['middleware'] as $middleware) {
     $middlewareQueue[] = $autowire->instantiateClass($middleware);
 }
